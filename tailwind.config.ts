88110a2a35d4cb56e5c38e8e15343f1b9d19afec
Pablo Moscon <mosconpablo@gmail.com>
2025-12-17 06:43:20 -0300

    Revert "Develop"

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        rosa: '#dbaaa2',
+        'rosa-claro': '#f2d5ce',
+        'gris-clarito': '#ededed',
+        'verde-azulado': '#527e7f',
+        'verde-claro': '#b2cebf',
+      },
+      fontFamily: {
+        asap: ['Asap', 'sans-serif'],
+        'retro-signature': ['Retro Signature', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
